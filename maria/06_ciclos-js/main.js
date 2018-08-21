a83915f32703009202cac69aac64e3676d25f7cb
maretluce <maretluce@gmail.com>
2018-08-21 12:22:44 -0500

    ejercicio 6

--- maria/06_ciclos-js/main.js
+++ maria/06_ciclos-js/main.js
@@ -4,4 +4,19 @@ Tarea: Mostrar si un número es par o impar.
 */
 
 //Captura el número
-num = parseInt(promt("Ingrese un número",0));
+var num = parseInt(prompt("Ingrese un número"));
+
+//Calcula el modulo
+var mod = num % 2;
+
+//Valida si num es número
+while(isNaN(num)){
+	num = parseInt(prompt("El valor ingresado NO es valido. Vuelva a ingresar un número."));
+}
+
+//Valida si num es par o impar
+if (mod == 0){
+	document.write("El número "+num+" es par");
+}else {
+	document.write("El número "+num+" es impar");
+}	
